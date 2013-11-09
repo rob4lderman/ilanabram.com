ef28c9e0bb00658cc5d5d127e8df1e5458e8e150
Rob Alderman <robertgalderman@gmail.com>
2013-11-09 09:41:03 -0500

    completed lla

---
+++ sync.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+# Usage: 
+# $ cd ilanabram.com
+# $ ./sync.sh <user> <pass>
+#
+# NOTE: as currently written, this script must be run from the source dir.
+#
+# NOTE: LFTP: if you specify a source-dir without a target-dir, then LFTP will append
+#       the basename of the source-dir to the target-dir name (since target-dir is unspecified,
+#       it takes on the root).  The same will happen if you specify a target-dir that ends
+#       in a "/" (except the root).
+#       
+#       E.g., 
+#           $ mirror --reverse .. /easy/ilanabram.com 
+#
+#       will create a dir called ilanabram.com on the target host and transfer all files
+#       under that directory.
+#
+#
+
+
+HOST=ilanabram.com
+LDIR=/easy/ilanabram.com
+RDIR=/
+USER="$1"
+PASS="$2"
+
+lftp -c "
+open $HOST
+user $USER $PASS
+mirror --reverse --exclude sync.sh --exclude .git --exclude-glob .git* --exclude-glob *.swp 
+"
+
+
